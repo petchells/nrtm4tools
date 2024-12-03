50d6b946c8a19eea40392156e2221caeb361af58
Paul Etchells (interceptor) <p.a.etchells@gmail.com>
2024-12-03 11:10:59 +0100

    removed unused definition

--- internal/nrtm4/util/mapper.go
+++ internal/nrtm4/util/mapper.go
@@ -1,14 +1,11 @@
 package util
 
-// HashMap is a generic definition of a map
-type HashMap[K comparable, E any] map[K]E
-
 // KeyFunction is a function that extracts a map key from an entity
 type KeyFunction[K comparable, E any] func(E) K
 
 // SliceToMap creates a map from a slice or array. fn should return the key for the map
-func SliceToMap[K comparable, E any](fn KeyFunction[K, E], vals []E) HashMap[K, E] {
-	m := HashMap[K, E]{}
+func SliceToMap[K comparable, E any](fn KeyFunction[K, E], vals []E) map[K]E {
+	m := map[K]E{}
 	for _, v := range vals {
 		m[fn(v)] = v
 	}
