e2bb3c2d5a41880b2d19024fbb6c5934ba77cce9
Paul Etchells (interceptor) <p.a.etchells@gmail.com>
2025-02-22 04:57:28 +0100

    sql works for update

--- third_party/tern/004_history_tables.sql
+++ third_party/tern/004_history_tables.sql
@@ -1,55 +1,93 @@
-create sequence _history_seq start 1;
+CREATE SEQUENCE _history_seq start 1
+;
 
-create table history_nrtm_rpslobject (
-    id bigint not null primary_key,
-    seq bigint not null,
-    `timestamp` timestamp without timezone,
-	old_id bigint not null,
-    object_type character varying(255) NOT NULL,
-    primary_key character varying(255) NOT NULL,
-    nrtm_source_id bigint NOT NULL,
-    from_version integer NOT NULL,
-    to_version integer NOT NULL,
-    rpsl text NOT NULL
-);
+CREATE TABLE history_nrtm_rpslobject (
+	id BIGINT NOT NULL PRIMARY KEY,
+	seq BIGINT NOT NULL,
+	stamp TIMESTAMP WITHOUT TIME ZONE,
+	old_id BIGINT NOT NULL,
+	object_type CHARACTER VARYING(255) NOT NULL,
+	primary_key CHARACTER VARYING(255) NOT NULL,
+	nrtm_source_id BIGINT NOT NULL,
+	version INTEGER NOT NULL,
+	rpsl TEXT NOT NULL
+)
+;
 
-alter table nrtm_rpslobject drop column to_version;
-alter table nrtm_rpslobject column to_version version;
+ALTER TABLE nrtm_rpslobject
+DROP COLUMN to_version
+;
 
-create unique index history_nrtm_rpslobject_seq__idx on history_nrtm_rpslobject(seq);
-create index history_nrtm_rpslobject_source__idx on history_nrtm_rpslobject(source);
-create index history_nrtm_rpslobject_type_key__idx on history_nrtm_rpslobject(object_type, primary_key);
+ALTER TABLE nrtm_rpslobject
+RENAME COLUMN from_version TO version
+;
 
-CREATE FUNCTION update_rpslobject_history() RETURNS trigger AS $rpsl_history_recorder$
+CREATE UNIQUE index history_nrtm_rpslobject_seq__idx ON history_nrtm_rpslobject (seq)
+;
+
+CREATE INDEX history_nrtm_rpslobject_source__idx ON history_nrtm_rpslobject (nrtm_source_id)
+;
+
+CREATE INDEX history_nrtm_rpslobject_type_key__idx ON history_nrtm_rpslobject (object_type, primary_key)
+;
+
+CREATE FUNCTION update_rpslobject_history () returns trigger AS $rpsl_history_recorder$
+    DECLARE
+        _seq bigint;
     BEGIN
         set timezone to 'UTC'; -- it should be anyway, but just in case
-        SELECT nextval('_history_seq') INTO seq_id;
+        SELECT nextval('_history_seq') INTO _seq;
         INSERT INTO history_nrtm_rpslobject
-            (seq, id, seq, object_type, primary_key, nrtm_source_id, from_version, to_version, rpsl)
+            (id, seq, stamp, old_id, object_type, primary_key, nrtm_source_id, version, rpsl)
         VALUES (
             id_generator(),
-            seq_id,
+            _seq,
             now(),
             OLD.id,
             OLD.object_type,
             OLD.primary_key,
             OLD.nrtm_source_id,
             OLD.version,
-            NEW.version,
             OLD.rpsl
         );
         RETURN NEW;
     END;
-$rpsl_history_recorder$ LANGUAGE plpgsql;
+$rpsl_history_recorder$ language plpgsql
+;
 
-CREATE TRIGGER update_rpsl_trigger BEFORE DELETE OR UPDATE ON nrtm_rpslobject
-    FOR EACH ROW EXECUTE FUNCTION update_rpslobject_history();
+CREATE TRIGGER update_rpsl_trigger before delete
+OR
+UPDATE ON nrtm_rpslobject FOR each ROW
+EXECUTE function update_rpslobject_history ()
+;
 
 ---- create above / drop below ----
 
-drop trigger update_rpsl_trigger;
-drop function update_rpslobject_history;
-drop index history_nrtm_rpslobject_type_key__idx;
-drop index history_nrtm_rpslobject_source__idx;
-drop index history_nrtm_rpslobject_seq__idx;
-drop sequence _history_seq;
+DROP TRIGGER update_rpsl_trigger ON nrtm_rpslobject
+;
+
+DROP FUNCTION update_rpslobject_history
+;
+
+DROP INDEX history_nrtm_rpslobject_type_key__idx
+;
+
+DROP INDEX history_nrtm_rpslobject_source__idx
+;
+
+DROP INDEX history_nrtm_rpslobject_seq__idx
+;
+
+ALTER TABLE nrtm_rpslobject
+ADD COLUMN to_version INTEGER NOT NULL
+;
+
+ALTER TABLE nrtm_rpslobject
+RENAME COLUMN version TO from_version
+;
+
+DROP TABLE history_nrtm_rpslobject
+;
+
+DROP SEQUENCE _history_seq
+;
